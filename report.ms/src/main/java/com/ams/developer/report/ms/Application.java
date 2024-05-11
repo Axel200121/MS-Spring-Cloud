529480e923b66a313fd6b9c91eb369eaccb05622
Axel200121 <axelmartinez212001@gmail.com>
2024-05-11 17:57:42 -0600

    eliminado la configrción de eureka cliente en main del ms de report

--- report.ms/src/main/java/com/ams/developer/report/ms/Application.java
+++ report.ms/src/main/java/com/ams/developer/report/ms/Application.java
@@ -1,8 +1,5 @@
 package com.ams.developer.report.ms;
 
-import com.netflix.discovery.EurekaClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,18 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class Application implements CommandLineRunner {
-
-	@Autowired
-	private EurekaClient eurekaClient;
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		this.eurekaClient.getAllKnownRegions().forEach(System.out::println);
-		System.out.println(this.eurekaClient.getApplications("companies-crud"));
-	}
 }
