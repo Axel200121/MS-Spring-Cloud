e965b24b2bde08c8a825dcdf86d6164bc7361b6e
Axel200121 <axelmartinez212001@gmail.com>
2024-05-18 16:05:17 -0600

    Creando el MS gateway

---
+++ gateway/src/test/java/com/ams/developer/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.developer.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
