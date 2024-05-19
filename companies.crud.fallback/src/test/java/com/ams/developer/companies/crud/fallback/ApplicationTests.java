4d03538ac9f7b7e4858716131581c04aa0dfa52f
Axel200121 <axelmartinez212001@gmail.com>
2024-05-19 09:41:53 -0600

    creando el ms de companies fallback

---
+++ companies.crud.fallback/src/test/java/com/ams/developer/companies/crud/fallback/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.developer.companies.crud.fallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
